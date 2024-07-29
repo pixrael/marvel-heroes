10277eb94d0e785c2f1d9e6c0f26115bad6ac712
Set Ortiz <pixrael777@gmail.com>
2024-07-29 14:17:57 +0200

    add search input with styles

--- src/components/search-character/search-character.tsx
+++ src/components/search-character/search-character.tsx
@@ -1,12 +1,18 @@
+import iconAmplifier from '../../assets/imgs/amplifier button.svg';
 import './search-character.scss';
 
 function SearchCharacter() {
+  const handleAmplifierClick = () => {
+    // console.log('logo clicked'); TODO
+  };
   return (
-    <div className="search-container">
-      <i className="search-icon"></i>
+    <div className="searchcontainer">
+      <button className="iconbtn" onClick={handleAmplifierClick}>
+        <img className="iconbtn__img" src={iconAmplifier} />
+      </button>
       <input
         type="text"
-        className="search-input"
+        className="searchcontainer__searchinput"
         placeholder="SEARCH A CHARACTER..."
       />
     </div>
