7ca20012f7949b86b3da120cf98351c410b75ed8
Set Ortiz <pixrael777@gmail.com>
2024-08-07 19:00:56 +0200

    componetize the character section

--- src/components/page-components/details-character-page-components/details-character-section/details-character-section.tsx
+++ src/components/page-components/details-character-page-components/details-character-section/details-character-section.tsx
@@ -1,48 +1,19 @@
-import './details-character-section.scss';
+import CharacterSection from '../../../ui-components/character-section/character-section';
 
 function DetailsCharacterSection() {
-  const handleFavoriteClick = () => {
-    console.log('favorite clicked');
-  };
   return (
-    <div className="section">
-      <div className="bodysection">
-        <div className="carddetail">
-          <div className="row">
-            <div className="colimage">
-              <img
-                src="http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
-                alt="Card image"
-              />
-            </div>
-            <div className="coldetails">
-              <div className="cardbody">
-                <div className="cardheader">
-                  <h5 className="cardtitle">Card title</h5>
-                  <button
-                    className="iconbtn iconbtn--medium"
-                    onClick={handleFavoriteClick}
-                  >
-                    <span className="hearticon"></span>
-                  </button>
-                </div>
-                <div className="cardtextcontainer">
-                  <p className="cardtext">
-                    This is a wider card with supporting text below as a natural
+    <CharacterSection
+      id={1}
+      title="Super HERO"
+      img="http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
+      handleFavoriteClick={(id: number) => console.log('favorite ', id)}
+      description="This is a wider card with supporting text below as a natural
                     lead-in to additional content. This content is a little bit
                     longer. This is a wider card with supporting text below as a
                     natural lead-in to additional content. This content is a
                     little bit longer. This is a wider card with supporting text
-                    below as a natural lead-in to additional content.
-                  </p>
-                </div>
-                <div className="corner"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+                    below as a natural lead-in to additional content."
+    />
   );
 }
 
---
+++ src/components/ui-components/character-section/character-section.tsx
@@ -0,0 +1,48 @@
+import './character-section.scss';
+
+function CharacterSection({
+  id,
+  title,
+  img,
+  description,
+  handleFavoriteClick,
+}: {
+  id: number;
+  title: string;
+  img: string;
+  description: string;
+  handleFavoriteClick: (id: number) => void;
+}) {
+  return (
+    <div className="section">
+      <div className="bodysection">
+        <div className="carddetail">
+          <div className="row">
+            <div className="colimage">
+              <img src={img} alt="Card image" />
+            </div>
+            <div className="coldetails">
+              <div className="cardbody">
+                <div className="cardheader">
+                  <h5 className="cardtitle">{title}</h5>
+                  <button
+                    className="iconbtn iconbtn--medium"
+                    onClick={() => handleFavoriteClick(id)}
+                  >
+                    <span className="hearticon"></span>
+                  </button>
+                </div>
+                <div className="cardtextcontainer">
+                  <p className="cardtext">{description}</p>
+                </div>
+                <div className="corner"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CharacterSection;
