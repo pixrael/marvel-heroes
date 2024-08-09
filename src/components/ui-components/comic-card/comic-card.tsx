5174a9805dfee8e2c05643dc69647caf5fc61bb4
Set Ortiz <pixrael777@gmail.com>
2024-08-09 18:28:16 +0200

    add comic list display

--- src/components/ui-components/comic-card/comic-card.tsx
+++ src/components/ui-components/comic-card/comic-card.tsx
@@ -1,7 +1,4 @@
-import { useNavigate } from 'react-router-dom';
 import './comic-card.scss';
-import { useContext } from 'react';
-import { FavoriteContext } from '../../../contexts/favorite-context';
 
 function ComicCard({
   id,
@@ -15,29 +12,12 @@ function ComicCard({
   year: string;
 }) {
   return (
-    <>
-      {id} - {title} - {year}
-    </>
-  );
-
-  /*  return (
-    <div className="herocard clickeable" onClick={() => handleCardClick(id)}>
-      <img className="herocard__media" src={img} alt="marvel image" />
-      <div className="herocard__content">
-        <h5 className="herocard__title">{name}</h5>
-        <button
-          className="iconbtn iconbtn--smaller"
-          onClick={(event) => {
-            event.stopPropagation();
-            handleFavoriteClick(id);
-          }}
-        >
-          <span className={isSelected ? 'heartlineicon' : 'hearticon'}></span>
-        </button>
-      </div>
-      <div className="corner"></div>
+    <div className="stacked-card">
+      <img src={img} alt={title} className="stacked-card__image" />
+      <div className="stacked-card__title">{title}</div>
+      <div className="stacked-card__year">{year}</div>
     </div>
-  ); */
+  );
 }
 
 export default ComicCard;
--- src/pages/details-character-page.tsx
+++ src/pages/details-character-page.tsx
@@ -1,6 +1,6 @@
 import Header from '../components/ui-components/header/header';
 import DetailsCharacterSection from '../components/page-components/details-character-page-components/details-character-section/details-character-section';
-import DetailsCharactersComicList from '../components/page-components/details-character-page-components/details-characters-comic-list/detailsl-characters-comic-list';
+import DetailsCharactersComicList from '../components/page-components/details-character-page-components/details-characters-comic-list/details-characters-comic-list';
 import Subtitle from '../components/ui-components/subtitle/subtitle';
 
 function DetailsCharactersPage() {
