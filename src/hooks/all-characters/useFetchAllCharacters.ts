2c421de50e4184ce22ea30c89c218ef360b2d750
Set Israel Ortiz Monasterio <pixrael777@gmail.com>
2024-08-14 12:44:19 +0200

    make request depending on the selected limit in the app setup

--- src/hooks/all-characters/useFetchAllCharacters.ts
+++ src/hooks/all-characters/useFetchAllCharacters.ts
@@ -1,12 +1,23 @@
-import { FETCH_ALL_CHACTER_LIMIT } from '../../constants';
+import { useContext } from 'react';
 import { useFetchMarvelData } from '../useFetchMarvelData';
+import { SetupAppContext } from '../../app-state/contexts/setup-app-context';
+import {
+  FETCH_ALL_CHACTER_LIMIT_LIGHT,
+  FETCH_ALL_CHACTER_LIMIT,
+} from '../../constants';
 
 export const useFetchAllCharacters = (heroToSearch?: string) => {
+  const { limitTo5Results } = useContext(SetupAppContext);
+
+  const limit = limitTo5Results
+    ? FETCH_ALL_CHACTER_LIMIT_LIGHT
+    : FETCH_ALL_CHACTER_LIMIT;
+
   let params: { limit: number; nameStartsWith?: string } = {
-    limit: FETCH_ALL_CHACTER_LIMIT,
+    limit,
   };
   if (heroToSearch) {
-    params = { limit: FETCH_ALL_CHACTER_LIMIT, nameStartsWith: heroToSearch };
+    params = { limit, nameStartsWith: heroToSearch };
   }
 
   const { data, isLoading, error } = useFetchMarvelData({
--- src/hooks/details-characters/useFetchComicsByCharacterId.ts
+++ src/hooks/details-characters/useFetchComicsByCharacterId.ts
@@ -1,12 +1,23 @@
-import { FETCH_COMICS_BY_CHACTER_ID_LIMIT } from '../../constants';
+import { useContext } from 'react';
+import { SetupAppContext } from '../../app-state/contexts/setup-app-context';
+import {
+  FETCH_COMICS_BY_CHACTER_ID_LIMIT,
+  FETCH_COMICS_BY_CHACTER_ID_LIMIT_LIGHT,
+} from '../../constants';
 import { useFetchMarvelData } from '../useFetchMarvelData';
 
 export const useFetchComicsByCharacterId = (id: number | undefined) => {
+  const { limitTo5Results } = useContext(SetupAppContext);
+
+  const limit = limitTo5Results
+    ? FETCH_COMICS_BY_CHACTER_ID_LIMIT_LIGHT
+    : FETCH_COMICS_BY_CHACTER_ID_LIMIT;
+
   const { data, isLoading, error } = useFetchMarvelData({
     resource: 'characters',
     id,
     subresource: 'comics',
-    params: { limit: FETCH_COMICS_BY_CHACTER_ID_LIMIT, orderBy: 'onsaleDate' },
+    params: { limit, orderBy: 'onsaleDate' },
   });
 
   return {
